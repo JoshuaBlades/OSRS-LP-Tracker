3cec82a10d5ae177c810e1ca7686e70c272ee4b3
Josh Blades <joshblades@outlook.com>
2019-12-13 20:12:27 +0000

    The main proj

---
+++ OSRSBossTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OSRSBossTracker.MvcApplication" Language="C#" %>
---
+++ OSRSBossTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OSRSBossTracker.Startup))]
+namespace OSRSBossTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
