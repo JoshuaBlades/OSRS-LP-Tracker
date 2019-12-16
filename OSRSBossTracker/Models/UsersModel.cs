19384f4bf458e193e03778cc945f7cc283c945c4
Jack Cracknell <jcracknell123@gmail.com>
2019-12-16 20:45:37 +0000

    Added the proper boss names on the stats page. Currently Cerberus and Callisto are missing from api, so they're not included.

--- OSRSBossTracker/Models/UsersModel.cs
+++ OSRSBossTracker/Models/UsersModel.cs
@@ -18,6 +18,12 @@ namespace OSRSBossTracker.Models
         public List<string> Stats = new List<string>();
 
         public List<Bosses> Bosses = new List<Bosses>();
+
+        public List<string> BossList = new List<string>() { "Abyssal Sire", "Alchemical Hydra", "Barrows Chests", "Bryophyta", "Chambers of Xeric", "Chambers of Xeric: Challenge Mode", "Chaos Elemental",
+           "Chaos Fanatic", "Commander Zilyana", "Corporeal Beast", "Crazy Archaeologist", "Dagannoth Prime","Dagannoth Rex", "Dagannoth Supreme", "Deranged Archeologist", "General Graardor", "Giant Mole", 
+            "Grotesque Guardians", "Hespori", "Kalphite Queen", "King Black Dragon", "Kraken", "Kree'Arra", "K'ril Tsutsaroth", "Mimic", "Obor",
+            "Sarachnis", "Scorpia", "Skotizo", "The Gauntlet", "The Corrupted Gauntlet", "Theatre of Blood", "Thermonuclear Smoke Devil", "TzKal-Zuk", "TzTok-Jad", "Venenatis",
+                                             "Vet'ion", "Vorkath", "Wintertodt", "Zalcano", "Zulrah"};
     }
 }
 public class Bosses
